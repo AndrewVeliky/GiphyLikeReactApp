26944d3db0ce5f171064acf0be092bcc82556add
unknown <andrew.veliky@gmail.com>
2018-05-15 10:13:11 +0300

    Change displaying of each Gif

--- src/Gif.js
+++ src/Gif.js
@@ -1,29 +1,31 @@
 import React, { Component } from 'react';
 import Col  from 'react-bootstrap/lib/Col';
 import Image from 'react-bootstrap/lib/Image';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 
-export default class Gif extends Component {
-    constructor(props){
-      super(props);
-    }
-    
+export default class Gif extends Component {    
     render() {
-      const signStyle ={
-        maxWidth: `${this.props.width}px`
-      };
-
-      return (  
-        <Col xs={6} md={4}>
-          <div>
-            <Image 
-              src={this.props.src} 
-              className="center-block"
-              //need to add saving on click on the download button 
-              responsive/>
-            <div className="gif-sign" style={signStyle}>{this.props.title}</div>
-          </div>
-        </Col>
+      return (
+        <MuiThemeProvider>  
+          <Col xs={12} md={4} lg={3}>
+            <Card >
+            <CardMedia
+                overlay={<CardTitle subtitle={this.props.title} />}
+                actAsExpander={true}
+            >
+              <Image 
+                src={this.props.src} 
+                className="center-block"
+                responsive/>
+              </CardMedia>
+              <CardText expandable={true} >
+                  <a href={this.props.src} download className="download-btn"><i className="fa fa-download"></i> Download</a>
+              </CardText>
+            </Card>
+          </Col>
+        </MuiThemeProvider>
       );
     }
   }
